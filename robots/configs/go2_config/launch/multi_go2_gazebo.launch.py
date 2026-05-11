2a07619ba62cd32e649b9bb4282083a3a6d5285d
tara-kas <tara@phuketcafe.com>
2026-05-11 15:56:35 +0100

    match name tags

--- robots/configs/go2_config/launch/multi_go2_gazebo.launch.py
+++ robots/configs/go2_config/launch/multi_go2_gazebo.launch.py
@@ -131,8 +131,7 @@ def generate_launch_description():
             )
 
             # D. Bridge Gazebo PointCloud to ROS 2
-            # Using 'slam_world' based on your ign topic -l output earlier
-            gz_lidar_topic = f'/world/slam_world/model/{name}/link/lidar_link/sensor/lidar/scan/points'
+            gz_lidar_topic = f'/world/slam_world/model/{name}/link/velodyne/sensor/velodyne-VLP16/scan/points'
             ros_lidar_topic = f'{namespace}/pointcloud'
 
             lidar_bridge = Node(
